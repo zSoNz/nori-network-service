72bde752dd8bcd607be61fe1b7f169a9b26d5e9f
Kikacheishvili Bogdan <bogdan.kikacheishvili@idapgroup.com>
2019-12-03 10:59:14 +0200

    Revert "Result type added."
    This reverts commit d40866c5058f607b90f41274d40f8d95ef64d8cb.

--- Pods/Target Support Files/Pods-NetworkService/Pods-NetworkService-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NetworkService : NSObject
-@end
-@implementation PodsDummy_Pods_NetworkService
-@end
--- Pods/Target Support Files/Pods-Nori Network Service/Pods-Nori Network Service-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Nori_Network_Service : NSObject
-@end
-@implementation PodsDummy_Pods_Nori_Network_Service
-@end
